0a3d3f394dc0384b5e9ba0ea3f8878c915314f8d
Admin Stagiaire <yannickpitaud@gmail.com>
2023-12-18 16:07:40 +0100

    Feat : Commit

--- src/AnotherClass.java
+++ src/AnotherClass.java
@@ -1,5 +1,8 @@
 public class AnotherClass {
     public static void main(String[] args) {
         System.out.println("Conflict solved");
+        System.out.println("Nouveau Conflit Resolu");
+        System.out.println("Nouvelle Modification Amélioré !");
+
     }
 }
