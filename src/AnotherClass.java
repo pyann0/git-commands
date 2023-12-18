dd67ea03ab1c632d8e9dd25d84fec23f578f5e68
Admin Stagiaire <yannickpitaud@gmail.com>
2023-12-18 14:27:09 +0100

    Feat : Create Conflict

--- src/AnotherClass.java
+++ src/AnotherClass.java
@@ -1,5 +1,5 @@
 public class AnotherClass {
     public static void main(String[] args) {
-        System.out.println("Dev Branch");
+        System.out.println("Conflict from Another Dev");
     }
 }
