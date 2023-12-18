fafa752514962a2b29af766c7f6dab7148d6b536
Admin Stagiaire <yannickpitaud@gmail.com>
2023-12-18 12:28:46 +0100

    Fix : Adding blank space

--- src/AnotherClass.java
+++ src/AnotherClass.java
@@ -1,5 +1,5 @@
 public class AnotherClass {
     public static void main(String[] args) {
-        System.out.println("AnotherClass.main");
+        System.out.println("AnotherClass.main ");
     }
 }
